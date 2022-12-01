39221a6a1ab3566175d9258542f7a8a8c61b6f90
Kimiru <laursim24@gmail.com>
2022-12-01 19:56:25 +0100

    Explicitly used window.setInterval
    Used window.setInterval to resolve some compilation issues.
    window.setInterval returns number
    (NodeJS) setInterval returns Node.Timer

--- ts/Network.ts
+++ ts/Network.ts
@@ -370,7 +370,7 @@ export class NetworkConnection {
         this.connection = connection
         this.receiver = receiver
 
-        this.intervalID = setInterval(this.#timeout.bind(this), 1000)
+        this.intervalID = window.setInterval(this.#timeout.bind(this), 1000)
 
         this.connection.on('open', this.#open.bind(this))
         this.connection.on('close', this.#close.bind(this))
